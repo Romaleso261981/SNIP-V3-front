cc7640e131e76feba046ae1a8f11849cb75ad2a9
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2025-01-31 10:57:49 +0200

    feat: added tests jest

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,26 @@
+// jest.config.js
 import nextJest from "next/jest";
 
 const createJestConfig = nextJest({
-  dir: "./"
+  dir: "./" // Ваш корінь проєкту
 });
 
 const customJestConfig = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"], // Якщо є файл з налаштуваннями для тестів
+  testEnvironment: "jest-environment-jsdom",
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1"
+    "^@/(.*)$": "<rootDir>/src/$1", // Маппінг для ваших псевдонімів шляхів
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy" // Для маппінгу стилів
   },
-  testEnvironment: "jest-environment-jsdom"
+  transform: {
+    "^.+\\.(ts|tsx)$": "babel-jest" // Використання babel для трансформації TypeScript та JSX
+  },
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}", // Включення всіх TypeScript файлів в покриття
+    "!src/pages/_app.tsx", // Не брати ці файли до покриття
+    "!src/pages/_document.tsx"
+  ],
+  coverageReporters: ["lcov", "text-summary"] // Формати покриття
 };
 
 export default createJestConfig(customJestConfig);
