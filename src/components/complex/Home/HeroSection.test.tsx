2a5721f204f4fcb6282e50bb82142e9d9383c5bc
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2025-03-26 08:55:35 +0200

    fix: test

--- src/components/complex/Home/HeroSection.test.tsx
+++ src/components/complex/Home/HeroSection.test.tsx
@@ -5,13 +5,15 @@ import { StrapiImage } from "@/types/apiStrapiTypes";
 describe("HeroSection", () => {
   const mockData = {
     title: "Вплети традицію у свій інтер'єр!",
-    video: {
-      url: "/test-image.jpg",
-    } as StrapiImage,
+    video: [
+      {
+        url: "/test-image.jpg",
+      },
+    ] as StrapiImage[],
   };
 
   it("renders the title text", () => {
-    render(<HeroSection title={mockData.title} video={mockData.video} />);
+    render(<HeroSection video={mockData.video[0]} title={mockData.title} />);
     expect(
       screen.getByText("Вплети традицію у свій інтер'єр!")
     ).toBeInTheDocument();
