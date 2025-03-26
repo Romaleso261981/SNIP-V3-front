5cc88e94de26040473ca567313f8d91111b39e69
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2025-03-26 09:04:03 +0200

    fix: test

--- src/components/complex/BuyFromUs/CardList.tsx
+++ src/components/complex/BuyFromUs/CardList.tsx
@@ -9,32 +9,32 @@ type CardListProps = {
 
 export default function CardList({ cards }: CardListProps) {
   return (
-    <div className="w-full text-center text-gold flex flex-wrap gap-3 justify-center border-gold mt-15 mb-14 md:my-5">
+    <div className='w-full text-center text-gold flex flex-wrap gap-3 justify-center border-gold mt-15 mb-14 md:my-5'>
       {cards.map((card: Card) => {
-        const imageUrl = getStrapiMedia(card.image.url) || "";
+        const imageUrl = getStrapiMedia(card.gallery.images[0].url) || "";
         return (
           <Link
             href={`product/${card.id}`}
             key={card.id}
-            className="flex items-center justify-center w-full md:w-1/4"
+            className='flex items-center justify-center w-full md:w-1/4'
           >
-            <div className="flex flex-col items-center justify-center w-full md:w-1/2 border border-gold p-4 rounded-sm shadow-lg transition-transform transform hover:scale-105 overflow-hidden group">
-              <div className="relative overflow-hidden w-full h-auto">
+            <div className='flex flex-col items-center justify-center w-full md:w-1/2 border border-gold p-4 rounded-sm shadow-lg transition-transform transform hover:scale-105 overflow-hidden group'>
+              <div className='relative overflow-hidden w-full h-auto'>
                 <Image
                   src={imageUrl}
-                  alt={card.image.url ? "Product image" : "Placeholder image"}
-                  width={card.image.width}
-                  height={card.image.height}
-                  className="transition-transform duration-500 transform group-hover:translate-x-[-10%]"
+                  alt={
+                    card.gallery.images[0].url
+                      ? "Product image"
+                      : "Placeholder image"
+                  }
+                  width={card.gallery.images[0].width}
+                  height={card.gallery.images[0].height}
+                  className='transition-transform duration-500 transform group-hover:translate-x-[-10%]'
                 />
               </div>
-              <div className="flex items-center gap-2 py-5 md:flex-col">
-                <h4 className="font-medium text-sm">
-                  {card.name}
-                </h4>
-                <span>
-                  {card.price} {card.union}
-                </span>
+              <div className='flex items-center gap-2 py-5 md:flex-col'>
+                <h4 className='font-medium text-sm'>{card.name}</h4>
+                <span>{card.price} грн</span>
               </div>
             </div>
           </Link>
