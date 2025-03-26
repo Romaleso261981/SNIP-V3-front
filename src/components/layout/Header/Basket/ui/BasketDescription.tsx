c300d4f9a40131242067f2eede66277d54884acd
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2025-03-26 09:10:47 +0200

    fix: test

--- src/components/layout/Header/Basket/ui/BasketDescription.tsx
+++ src/components/layout/Header/Basket/ui/BasketDescription.tsx
@@ -25,7 +25,7 @@ const BasketDescription: FC<BasketDescriptionProps> = ({ router }) => {
     getTotalCount,
     increaseCount,
     removeCardById,
-    handleToggleBasket
+    handleToggleBasket,
   } = useContext(BasketContext);
 
   const { setParchaseList } = useContext(CheckOutContext);
@@ -39,96 +39,76 @@ const BasketDescription: FC<BasketDescriptionProps> = ({ router }) => {
 
   if (basketItems.length === 0) {
     return (
-      <div className="fixed h-full top-0 right-0 p-2 z-20 bg-white text-gold md:w-2/6 md:p-6">
-        <div className="flex flex-col justify-between items-start text-center p-2 pb-8 h-full">
+      <div className='fixed h-full top-0 right-0 p-2 z-20 bg-white text-gold md:w-2/6 md:p-6'>
+        <div className='flex flex-col justify-between items-start text-center p-2 pb-8 h-full'>
           <div
-            className="flex gap-5 flex-row-reverse mb-10 pl-3 pt-3 md:p-0"
+            className='flex gap-5 flex-row-reverse mb-10 pl-3 pt-3 md:p-0'
             onClick={() => handleToggleBasket()}
           >
-            <Image src={cross} alt="cross" />
+            <Image src={cross} alt='cross' />
           </div>
-          <h2 className="mb-5 text-xl">Ваша корзина пуста</h2>
+          <h2 className='mb-5 text-xl'>Ваша корзина пуста</h2>
         </div>
       </div>
     );
   }
 
   return (
-    <div className="fixed h-full top-0 right-0 p-2 z-20 bg-white text-gold md:w-2/6 md:p-6">
-      <div className="flex flex-col justify-between items-start text-center p-2 pb-8 h-full">
+    <div className='fixed h-full top-0 right-0 p-2 z-20 bg-white text-gold md:w-2/6 md:p-6'>
+      <div className='flex flex-col justify-between items-start text-center p-2 pb-8 h-full'>
         <div
-          className="flex gap-5 flex-row-reverse mb-10 pl-3 pt-3 md:p-0"
+          className='flex gap-5 flex-row-reverse mb-10 pl-3 pt-3 md:p-0'
           onClick={() => handleToggleBasket()}
         >
-          <Image src={cross} alt="cross" />
+          <Image src={cross} alt='cross' />
         </div>
-        <h2 className="mb-5 text-xl">
-          {t("mainTitle")}
-        </h2>
-        <div className="flex flex-col justify-start items-start w-full flex-grow overflow-auto">
-          {basketItems.map((card, index) =>
+        <h2 className='mb-5 text-xl'>{t("mainTitle")}</h2>
+        <div className='flex flex-col justify-start items-start w-full flex-grow overflow-auto'>
+          {basketItems.map((card, index) => (
             <div
               key={index}
-              className="flex justify-between items-center w-full mb-5 border-gold border-b-2 pb-4"
+              className='flex justify-between items-center w-full mb-5 border-gold border-b-2 pb-4'
             >
-              <BasketImage image={card.images[0].url} />
-              <div className="w-full h-full flex pl-4 flex-col items-start">
-                <p className="pb-8">
-                  {card.name}
-                </p>
-                <div className="w-full flex flex-row pb-1">
-                  <h4>
-                    {t("count")}
-                  </h4>
-                  <div className="flex flex-row items-center gap-3 px-6 pb-5">
+              <BasketImage image={card.gallery[0]} />
+              <div className='w-full h-full flex pl-4 flex-col items-start'>
+                <p className='pb-8'>{card.name}</p>
+                <div className='w-full flex flex-row pb-1'>
+                  <h4>{t("count")}</h4>
+                  <div className='flex flex-row items-center gap-3 px-6 pb-5'>
                     <button
                       onClick={() => increaseCount(card.id)}
-                      className="flex justify-center items-center rounded-md w-7 h-7 border-collapse border-gold border-2"
+                      className='flex justify-center items-center rounded-md w-7 h-7 border-collapse border-gold border-2'
                     >
                       +
                     </button>
-                    <p className="text-black text-base">
-                      {card.count}
-                    </p>
+                    <p className='text-black text-base'>{card.price}</p>
                     <button
                       onClick={() => decreaseCount(card.id)}
-                      className="flex justify-center items-center rounded-md w-7 h-7 border-collapse border-gold border-2"
+                      className='flex justify-center items-center rounded-md w-7 h-7 border-collapse border-gold border-2'
                     >
                       -
                     </button>
                   </div>
                 </div>
-                <div className="flex flex-row pb-5 gap-2">
-                  <h4>
-                    {t("price")}
-                  </h4>
-                  <p>
-                    {card.price}
-                  </p>
-                  <span>
-                    {card.union}
-                  </span>
+                <div className='flex flex-row pb-5 gap-2'>
+                  <h4>{t("price")}</h4>
+                  <p>{card.price}</p>
+                  <span>грн</span>
                 </div>
                 <button onClick={() => removeCardById(card.id)}>
                   {t("cardbuttonText")}
                 </button>
               </div>
             </div>
-          )}
+          ))}
         </div>
-        <div className="flex w-full gap-1 flex-row py-6">
-          <h4 className="">
-            {t("toBePaid")}
-          </h4>
-          <p className="text-black text-base">
-            {getTotalCount()}
-          </p>
-          <span>
-            {t("totalCoutUnion")}
-          </span>
+        <div className='flex w-full gap-1 flex-row py-6'>
+          <h4 className=''>{t("toBePaid")}</h4>
+          <p className='text-black text-base'>{getTotalCount()}</p>
+          <span>{t("totalCoutUnion")}</span>
         </div>
         <div onClick={() => checkOut(basketItems)}>
-          <button className="flex justify-center items-center w-full h-12 px-2 text-md bg-gold text-[#fff]">
+          <button className='flex justify-center items-center w-full h-12 px-2 text-md bg-gold text-[#fff]'>
             {t("buttonText")}
           </button>
         </div>
